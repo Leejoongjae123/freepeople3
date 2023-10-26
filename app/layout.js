85c86ab68cc217e5106c611166887ba5e8018819
ljj33 <ljj3347@naver.com>
2023-10-26 16:31:16 +0900

    56th commit

--- app/layout.js
+++ app/layout.js
@@ -2,23 +2,29 @@
 import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-
+import Head from 'next/head'
 import Provider from './components/Provider'
 
-export default function RootLayout({ children}) {
+export const metadata={
+  title:'미래민중',
+  description:"오직 민중을 위한"
+}
 
-  
+export default function RootLayout({ children}) { 
   
   return (
     <html lang="kr">
       <Head>
-        <title>자유민중</title>
+        <title>미래민중</title>
         <meta name="description" content="자유민중" />
       </Head>
+      
       <body className="">
         <Provider>
         <Navbar></Navbar>
+        
         <main className='grow'>
+        
         {children}
         </main>
         {/* <Provider> */}
--- app/page.js
+++ app/page.js
@@ -1,98 +1,83 @@
-'use client'
-import Image from 'next/image'
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import dynamic from 'next/dynamic'
-import Link from 'next/link'
+"use client";
+import Image from "next/image";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import dynamic from "next/dynamic";
+import Link from "next/link";
 // import ApexCharts from "react-apexcharts";
-import Review from './components/Review'
-import JijidoReal from './components/JijidoReal'
-import JijidoNBS from './components/JijidoNBS'
-import {FcVoicePresentation} from 'react-icons/fc'
-import { Element } from 'react-scroll' 
-import LoginBtn from './components/LoginBtn'
-import President from './components/President'
-import Parliament from './components/Parliament'
-import { FcDocument } from 'react-icons/fc'
-import Policy from './components/Policy'
-import Election from './components/Election'
-import { Song_Myung } from 'next/font/google'
-import { East_Sea_Dokdo } from 'next/font/google'
-import Head from 'next/head'
-const SongMyung=Song_Myung({subsets:['latin'],weight:['400']})
-const EastSeaDokdo=East_Sea_Dokdo({subsets:['latin'],weight:['400']})
+import Review from "./components/Review";
+import JijidoReal from "./components/JijidoReal";
+import JijidoNBS from "./components/JijidoNBS";
+import { FcVoicePresentation } from "react-icons/fc";
+import { Element } from "react-scroll";
+import LoginBtn from "./components/LoginBtn";
+import President from "./components/President";
+import Parliament from "./components/Parliament";
+import { FcDocument } from "react-icons/fc";
+import Policy from "./components/Policy";
+import Election from "./components/Election";
+import { Song_Myung } from "next/font/google";
+import { East_Sea_Dokdo } from "next/font/google";
+import Head from "next/head";
+const SongMyung = Song_Myung({ subsets: ["latin"], weight: ["400"] });
+const EastSeaDokdo = East_Sea_Dokdo({ subsets: ["latin"], weight: ["400"] });
 
 function classNames(...classNamees) {
-  return classNamees.filter(Boolean).join(' ')
+  return classNamees.filter(Boolean).join(" ");
 }
 
 export default function Home() {
-
   return (
     <div>
+      <Head>
+        <title>미래민중</title>
+        <meta name="description" content="자유민중" />
+      </Head>
 
-    
-    <Element name="0"></Element>
-    <div className="relative py-1 lg:py-6">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto  max-w-2xl text-center mt-16">
-          {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">정치/경제 종합 정보 제공</h2> */}
-          <p style={EastSeaDokdo.style} className="mt-3 font-bold text-4xl lg:text-6xl leading-8 text-sky-900">
-            오직 민중을 위한
-          </p>
-          <h1 className="my-2 lg:my-5 font-bold tracking-tight text-gray-900 lg:text-6xl ">
-            <p style={SongMyung.style} className="text-6xl lg:text-8xl">미 래 민 중</p>
-          </h1>
-          
+      <Element name="0"></Element>
+      <div className="relative py-1 lg:py-6">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto  max-w-2xl text-center mt-16">
+            {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">정치/경제 종합 정보 제공</h2> */}
+            <p
+              style={EastSeaDokdo.style}
+              className="mt-3 font-bold text-4xl lg:text-6xl leading-8 text-sky-900"
+            >
+              오직 민중을 위한
+            </p>
+            <h1 className="my-2 lg:my-5 font-bold tracking-tight text-gray-900 lg:text-6xl ">
+              <p style={SongMyung.style} className="text-6xl lg:text-8xl">
+                미 래 민 중
+              </p>
+            </h1>
+          </div>
         </div>
       </div>
-    </div>
 
+      <Element name="1"></Element>
+      <div id="mySection" className="mx-auto px-10 lg:px-40 pb-10 bg-gray-100">
+        <Review></Review>
+      </div>
 
-    <Element name="1"></Element>
-    <div id="mySection" className="mx-auto px-10 lg:px-40 pb-10 bg-gray-100">
-      <Review></Review>
-    </div>
+      <Element name="2"></Element>
+      <div className="bg-white px-10 lg:px-20 py-10">
+        <President></President>
+      </div>
 
-    <Element name="2"></Element>
-    <div className="bg-white px-10 lg:px-20 py-10">
-      <President></President>
-    </div>
-    
-    <Element name="3"></Element>
-    <div className="bg-gray-100 px-10 py-10 lg:px-20">
-      <Parliament></Parliament>
-    </div>
+      <Element name="3"></Element>
+      <div className="bg-gray-100 px-10 py-10 lg:px-20">
+        <Parliament></Parliament>
+      </div>
 
-    
-    <Element name="4"></Element>
-    <div className="bg-white-100 px-10 lg:px-20 py-10">
-      <Policy></Policy>
-    </div>
+      <Element name="4"></Element>
+      <div className="bg-white-100 px-10 lg:px-20 py-10">
+        <Policy></Policy>
+      </div>
 
-
-    <Element name="5"></Element>
-    <div className="bg-gray-100 px-10 lg:px-20 py-10">
-      <Election></Election>
+      <Element name="5"></Element>
+      <div className="bg-gray-100 px-10 lg:px-20 py-10">
+        <Election></Election>
+      </div>
     </div>
-    
-    </div>
-  
-
-    
-    
-    
-  )
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
